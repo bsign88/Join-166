742f205792567619fc3683f2466f68730bb59e3b
Marco Mundt <147448837+Mundt-Werk@users.noreply.github.com>
2024-04-19 13:47:19 +0200

    step eight include html

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let element of includeElements) {
+        let file = element.getAttribute("w3-include-html");
+        try {
+            let response = await fetch(file);
+            if (response.ok) {
+                element.innerHTML = await response.text();
+            } else {
+                element.innerHTML = 'Page not found';
+            }
+        } catch (error) {
+            element.innerHTML = 'Error loading the page';
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", includeHTML);
