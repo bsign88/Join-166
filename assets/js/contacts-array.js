72c959e5b70d1c848ba937e352d2cfb07b1105e7
Adrian-Arne Kolb <mauskrieger@hotmail.com>
2024-04-27 17:55:56 +0300

    first commit js

---
+++ assets/js/contacts-array.js
@@ -0,0 +1,43 @@
+let contacts = [
+{   "name": "Anton Mayer",
+    "email": "anton@gmail.com",
+    "phone": "+49 111 111 111",
+    "color": 'var(--color1)',
+},
+{
+    "name": "Anja Schulz",
+    "email": "schulz@gmail.com",
+    "phone": "+49 222 222 222",
+    "color": 'var(--color2)',
+},
+{
+    "name": "Benedikt Ziegler",
+    "email": "benedikt@gmail.com",
+    "phone": "+49 333 333 333",
+    "color": 'var(--color3)',
+},
+{
+    "name": "David Eisenberg",
+    "email": "davidberg@gmail.com",
+    "phone": "+49 444 444 444",
+    "color": 'var(--color4)',
+},
+{
+    "name": "Eva Fischer",
+    "email": "eva@gmail.com",
+    "phone": "+49 555 555 555",
+    "color": 'var(--color5)',
+},
+{
+    "name": "Emmanuel mauer",
+    "email": "emmanuelma@gmail.com",
+    "phone": "+49 666 666 666",
+    "color": 'var(--color6)',
+},
+{
+    "name": "Manuel Bauer",
+    "email": "bauer@gmail.com",
+    "phone": "+49 777 777 777",
+    "color": 'var(--color7)',
+}
+];
---
+++ assets/js/contacts.js
@@ -0,0 +1,39 @@
+let contacts;
+
+// Sortiere die Kontakte alphabetisch nach dem Namen
+contacts.sort((a, b) => (a.name > b.name) ? 1 : -1);
+
+// Funktion zur Anzeige der Kontaktliste
+function renderContacts() {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let contactsDiv = document.getElementById('contacts-list');
+
+    alphabet.forEach(letter => {
+        let contactsByLetter = contacts.filter(contact => contact.name.toLowerCase().startsWith(letter));
+
+        if (contactsByLetter.length > 0) {
+            let alphabetDiv = document.createElement('div');
+            alphabetDiv.classList.add('contacts-list-alphabet');
+            alphabetDiv.textContent = letter.toUpperCase();
+            contactsDiv.appendChild(alphabetDiv);
+
+            let separatorDiv = document.createElement('div');
+            separatorDiv.classList.add('contacts-list-separator');
+            contactsDiv.appendChild(separatorDiv);
+
+            let contactsByLetterDiv = document.createElement('div');
+            contactsByLetterDiv.id = `${letter}-contacts`;
+            contactsDiv.appendChild(contactsByLetterDiv);
+
+            contactsByLetter.forEach(contact => {
+                let contactDiv = document.createElement('div');
+                contactDiv.classList.add('contact');
+                contactDiv.textContent = contact.name;
+                contactsByLetterDiv.appendChild(contactDiv);
+            });
+        }
+    });
+}
+
+// Aufruf der Funktion zum Rendern der Kontaktliste
+renderContacts();
