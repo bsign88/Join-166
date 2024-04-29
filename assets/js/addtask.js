7699464e8a3be6caa6b44bc1a010b5628d0699ad
Björn Bangert <136905089+bsign88@users.noreply.github.com>
2024-04-29 23:02:00 +0200

    addtask js

---
+++ assets/js/addtask.js
@@ -0,0 +1,36 @@
+// Fügt To dos hinzu
+
+function addTask() {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    //let assignedto = document.getElementById('assignedto');
+    let date = document.getElementById('date').value;
+    //prio
+    let category = document.getElementById('category').value;
+    let subtask = document.getElementById('subtask').value;
+
+    console.log('Titel: ', title);
+    console.log('Beschreibung: ', description);
+    console.log('Datum: ', date);
+    console.log('Kategorie: ', category);
+    console.log('Subtask: ', subtask);
+
+    let task = {
+        'title': title,
+        'decription': description,
+        'date': date,
+        'category': category
+    }
+}
+
+
+// Öffnet das dropdown "Assigned to" bei Addtask
+
+function openDropdown() {
+    var checkList = document.getElementById("assignedto");
+    checkList.getElementsByClassName("anchor")[0].onclick = function (evt) {
+      if (checkList.classList.contains("visible"))
+        checkList.classList.remove("visible");
+      else checkList.classList.add("visible");
+    };
+  }
--- assets/js/script.js
+++ assets/js/script.js
@@ -94,14 +94,3 @@ function autoFillEmail() {
   passwordInput.type = 'password';
   passwordIcon.src = './assets/img/icons/visibility_off.png'; 
 }
-
-// Öffnet das dropdown "Assignet to" bei Addtask
-
-function openDropdown() {
-    var checkList = document.getElementById("list1");
-    checkList.getElementsByClassName("anchor")[0].onclick = function (evt) {
-      if (checkList.classList.contains("visible"))
-        checkList.classList.remove("visible");
-      else checkList.classList.add("visible");
-    };
-  }
