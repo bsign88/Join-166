3e9ba48d2b0604752e481febb1eede3f2834da07
Björn Bangert <136905089+bsign88@users.noreply.github.com>
2024-05-04 17:32:17 +0200

    radio buttons

--- assets/js/addtask.js
+++ assets/js/addtask.js
@@ -37,3 +37,39 @@ function openDropdown() {
       else checkList.classList.add("visible");
     };
   }
+
+  function showContactsDropdown() {
+
+
+  }
+
+  
+  function changeUrgentIcon() {
+    let urgent = document.getElementById('urgent');
+    if (urgent.checked) {
+      document.getElementById('urgent-icon').src = "./assets/img/icons/prio_urgent_active.svg";
+    } else {
+      document.getElementById('medium-icon').src = "./assets/img/icons/prio_medium_default.svg";
+      document.getElementById('low-icon').src = "./assets/img/icons/prio_low_default.svg";
+    }
+  }
+
+  function changeMediumIcon() {
+    let medium = document.getElementById('medium');
+    if (medium.checked) {
+      document.getElementById('medium-icon').src = "./assets/img/icons/prio_medium_active.svg";
+    } else {
+      document.getElementById('urgent-icon').src = "./assets/img/icons/prio_urgent_default.svg";
+      document.getElementById('low-icon').src = "./assets/img/icons/prio_low_default.svg";
+    }
+  }
+
+  function changeLowIcon() {
+    let low = document.getElementById('low');
+    if (low.checked) {
+      document.getElementById('low-icon').src = "./assets/img/icons/prio_low_active.svg";
+    } else {
+      document.getElementById('urgent-icon').src = "./assets/img/icons/prio_urgent_default.svg";
+      document.getElementById('medium-icon').src = "./assets/img/icons/prio_medium_default.svg";
+    }
+  }
--- assets/js/storage.js
+++ assets/js/storage.js
@@ -2,6 +2,7 @@ const STORAGE_TOKEN = 'MIIEDSNHP1WLD4CM84PW1A7YLVKDMMJ5BQRZ6PYI';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 let contacts;
 let userId;
+let tasks;
 
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
